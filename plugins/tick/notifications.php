2aaf1f15428e15ab12e98a5817e5d368cdf66827
shibdib <supernfadmin@gmail.com>
2016-03-11 18:34:51 -0500

    Suppress non error

--- plugins/tick/notifications.php
+++ plugins/tick/notifications.php
@@ -1,5 +1,29 @@
 <?php
 /**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Robert Sardinia
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
  * Class notifications
  */
 class notifications
@@ -156,7 +180,7 @@ class notifications
                             $msg = "War declared by {$aggressorAllianceName} has been invalidated. Fighting ends in roughly 24 hours.";
                             break;
                         case 71: // Mission Expiration
-                            $msg = null;
+                            $msg = "skip";
                             break;
                         case 75: // POS / POS Module under attack
                             $aggressorAllianceID = trim(explode(": ", $notificationString[0])[1]);
@@ -248,7 +272,8 @@ class notifications
                             break;
                     }
 
-                    if ($msg == null){
+                    /** @noinspection PhpUndefinedVariableInspection */
+                    if ($msg == "skip"){
                         return null;
                     }
                     $this->discord->api("channel")->messages()->create($this->toDiscordChannel, $msg);
