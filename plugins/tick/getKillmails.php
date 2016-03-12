3197d7fb3f4db14cdfa6770d51e259305c955c71
shibdib <supernfadmin@gmail.com>
2016-03-11 19:03:56 -0500

    Kill posting cap, to avoid websocket timeout

--- plugins/tick/getKillmails.php
+++ plugins/tick/getKillmails.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Robert Sardinia
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 /**
  * Class getKillmails
@@ -90,27 +113,37 @@ class getKillmails
         $url = "https://zkillboard.com/api/xml/no-attackers/no-items/orderDirection/asc/afterKillID/{$lastMail}/corporationID/{$this->corpID}";
         $xml = simplexml_load_file($url);
         $kills = $xml->result->rowset->row;
+        $i = 0;
+        $limit = 6;
         foreach ($kills as $kill) {
-            $killID = $kill->attributes()->killID;
-            $solarSystemID = $kill->attributes()->solarSystemID;
-            $systemName = dbQueryField("SELECT solarSystemName FROM mapSolarSystems WHERE solarSystemID = :id", "solarSystemName", array(":id" => $solarSystemID), "ccp");
-            $killTime = $kill->attributes()->killTime;
-            $victimAllianceName = $kill->victim->attributes()->allianceName;
-            $victimName = $kill->victim->attributes()->characterName;
-            $victimCorpName = $kill->victim->attributes()->corporationName;
-            $victimShipID = $kill->victim->attributes()->shipTypeID;
-            $shipName = dbQueryField("SELECT typeName FROM invTypes WHERE typeID = :id", "typeName", array(":id" => $victimShipID), "ccp");
-            // Check if it's a structure
-            if ($victimName != "") {
-                $msg = "**{$killTime}**\n\n**{$shipName}** flown by **{$victimName}** of (***{$victimCorpName}|{$victimAllianceName}***) killed in {$systemName}\nhttps://zkillboard.com/kill/{$killID}/";
-            }
-            elseif ($victimName == ""){
-                $msg = "**{$killTime}**\n\n**{$shipName}** of (***{$victimCorpName}|{$victimAllianceName}***) killed in {$systemName}\nhttps://zkillboard.com/kill/{$killID}/";
-            }
-            $this->discord->api("channel")->messages()->create($this->kmChannel, $msg);
-            setPermCache("newestKillmailID", $killID);
+            if ($i < $limit){
+                $killID = $kill->attributes()->killID;
+                $solarSystemID = $kill->attributes()->solarSystemID;
+                $systemName = dbQueryField("SELECT solarSystemName FROM mapSolarSystems WHERE solarSystemID = :id", "solarSystemName", array(":id" => $solarSystemID), "ccp");
+                $killTime = $kill->attributes()->killTime;
+                $victimAllianceName = $kill->victim->attributes()->allianceName;
+                $victimName = $kill->victim->attributes()->characterName;
+                $victimCorpName = $kill->victim->attributes()->corporationName;
+                $victimShipID = $kill->victim->attributes()->shipTypeID;
+                $shipName = dbQueryField("SELECT typeName FROM invTypes WHERE typeID = :id", "typeName", array(":id" => $victimShipID), "ccp");
+                // Check if it's a structure
+                if ($victimName != "") {
+                    $msg = "**{$killTime}**\n\n**{$shipName}** flown by **{$victimName}** of (***{$victimCorpName}|{$victimAllianceName}***) killed in {$systemName}\nhttps://zkillboard.com/kill/{$killID}/";
+                }
+                elseif ($victimName == ""){
+                    $msg = "**{$killTime}**\n\n**{$shipName}** of (***{$victimCorpName}|{$victimAllianceName}***) killed in {$systemName}\nhttps://zkillboard.com/kill/{$killID}/";
+                }
+                $this->discord->api("channel")->messages()->create($this->kmChannel, $msg);
+                setPermCache("newestKillmailID", $killID);
 
-            sleep (5);
+                sleep (1);
+                $i++;
+            }
+            else {
+                $updatedID = getPermCache("newestKillmailID");
+                $this->logger->info("Kill posting cap reached, newest kill id is {$updatedID}");
+                return null;
+            }
         }
         $updatedID = getPermCache("newestKillmailID");
         $this->logger->info("All kills posted, newest kill id is {$updatedID}");
