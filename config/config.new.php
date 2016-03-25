53ccfcc04672a333d143f72ecfa938b59630f559
shibdib <supernfadmin@gmail.com>
2016-03-25 05:05:24 -0400

    siphon detection

--- config/config.new.php
+++ config/config.new.php
@@ -98,13 +98,16 @@ $config["enabledPlugins"] = array(
 
 // Example from the 4M server
 $config["plugins"] = array(
+    //Checks if the server is up. Prone to hiccups if the api/crest go down
     "periodicTQStatus" => array(
         "channelID" => 118441700157816838 // what channel do u want server status reported too
     ),
+    //uses the provided api's to post evemails to a channel
     "evemails" => array(
         "fromIDs" => array(98047305, 99005805), // fill in with id's you want info from (have to be accessible with the api)
         "channelID" => 120639051261804544 // what channel id like these to post too
     ),
+    // Advanced plugin, read the wiki!!
     "fileReader" => array( // Documentation on this will be added to the wiki, used for pulling and posting jabber pings
         "db" => "/tmp/discord.db", // what file is read, DO NOT CHANGE.
         "channelConfig" => array(
@@ -131,18 +134,23 @@ $config["plugins"] = array(
             )
         ),
     ),
+    // what channel for eve notifications
     "notifications" => array(
-        "channelID" => 149918425018400768 // what channel for eve notifications
+        "channelID" => 149918425018400768 
     ),
+    //Spam twitter messages from people you follow to this channel
     "twitterOutput" => array(
         "channelID" => 120474010109607937 // twitter output channel
     ),
+    //Ask you bot questions using !wolf
     "wolframAlpha" => array(
         "appID" => "" // get an appID for wolframAlpha if you want to use the !wolf command
     ),
+    //Pricecheck tool
     "priceChecker" => array(
         "channelID" => "" //If you want to restrict price checker from working in a channel, put that channel's ID here.
     ),
+    //SSO Auth
     "auth" => array(
         "corpid" => "",
         "allianceID" => "0", // If you'd like to auth base on alliance put the alliance ID here.. also works to set blues.. DOES NOT WORK WITH API AUTH
@@ -154,9 +162,11 @@ $config["plugins"] = array(
         "nameEnforce" => "false", // put "true" or "false", if you'd like to make sure people's name match character names
         "url" => "" // put a url here if using sso auth too your sso page.
     ),
+    //Save fits to be displayed with the !fit command
     "saveFits" => array(
         "channel" => 12345 //Restrict saving/deleting fits to this channel. Use this to control who has access to altering fits. Use the channel ID.
     ),
+    //Killmail posting
     "getKillmails" => array(
         "channel" => "", //killmails post to this channel
         "corpID" => "", //corpid for killmails
@@ -164,5 +174,12 @@ $config["plugins"] = array(
         "lossMails" => "true", //set as true to post both kills and losses, false to post only kills.
         "spamAmount" => "10", //Max amount of kills the bot will post every 15 minutes. Default is 10 and won't get the bot kicked for spamming.
         "startMail" => 1, //Put the zkill killID of your latest killmail. Otherwise it will pull from the beginning of time.
-    )
+    ),
+    //Siphon detection works by looking for multiples of 100 inside standard silos. So if you take out a weird number it will trigger false positives.
+    "siphons" => array( 
+        "channelID" => "", //Siphon alerts post to this channel
+        "keyID" => "", //corp api keyID (Must have assets)
+        "vCode" => "", //corp api vCode
+        "prefix" => "", //put @everyone if you'd like everyone to be pinged when a siphon is detected
+)
 );
---
+++ plugins/tick/siphons.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Robert Sardinia
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * Class siphons
+ */
+class siphons {
+    /**
+     * @var
+     */
+    var $config;
+    /**
+     * @var
+     */
+    var $discord;
+    /**
+     * @var
+     */
+    var $logger;
+    /**
+     * @var
+     */
+    var $nextCheck;
+    /**
+     * @var
+     */
+    var $toIDs;
+    /**
+     * @var
+     */
+    var $toDiscordChannel;
+
+    /**
+     * @var
+     */
+    var $newestMailID;
+    /**
+     * @var
+     */
+    var $maxID;
+    /**
+     * @var
+     */
+    var $keyCount;
+    /**
+     * @var
+     */
+    var $keys;
+
+    /**
+     * @param $config
+     * @param $discord
+     * @param $logger
+     */
+    function init($config, $discord, $logger)
+    {
+        $this->config = $config;
+        $this->discord = $discord;
+        $this->logger = $logger;
+        $this->toDiscordChannel = $config["plugins"]["siphons"]["channelID"];
+        $this->keyID = $config["plugins"]["siphons"]["keyID"];
+        $this->vCode = $config["plugins"]["siphons"]["vCode"];
+        $this->prefix = $config["plugins"]["siphons"]["prefix"];
+        if (2 > 1) {
+            // Schedule it for right now
+            setPermCache("siphonLastChecked{$this->keyID}", time() - 5);
+        }
+    }
+
+    /**
+     *
+     */
+    function tick()
+    {
+        $lastChecked = getPermCache("siphonLastChecked{$this->keyID}");
+        $keyID = $this->keyID;
+        $vCode = $this->vCode;
+
+        if ($lastChecked <= time()) {
+            $this->logger->info("Checking API Key {$keyID} for siphons");
+            $this->checkTowers($keyID, $vCode);
+            //1 hour, cache is for 6 but good to notify hourly
+            setPermCache("siphonLastChecked{$keyID}", time() + 3600);
+        }
+    }
+
+    function checkTowers($keyID, $vCode)
+    {
+        $url = "https://api.eveonline.com/corp/AssetList.xml.aspx?keyID={$keyID}&vCode={$vCode}";
+        $xml = simplexml_load_file($url);
+
+        foreach ($xml->result->rowset->row as $structures){
+            //Check silos
+            if ($structures->attributes()->typeID == 14343){
+                foreach ($structures->rowset->row as $silo){
+                    //Avoid reporting empty silos
+                    if ($silo->attributes()->quantity != 0){
+                        //Check for a multiple of 100
+                        if ($silo->attributes()->quantity % 100 != 0) {
+                            $systemName = dbQueryField("SELECT solarSystemName FROM mapSolarSystems WHERE solarSystemID = :id", "solarSystemName", array(":id" => $structures->attributes()->locationID), "ccp");
+                            $msg = "{$this->prefix}";
+                            $msg .= "**POSSIBLE SIPHON**\n";
+                            $msg .= "**System: **{$systemName}\n";
+                            // Send the mails to the channel
+                            $this->discord->api("channel")->messages()->create($this->toDiscordChannel, $msg);
+                            sleep(1); // Lets sleep for a second, so we don't rage spam
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    function onMessage()
+    {
+    }
+
+    /**
+     * @return array
+     */
+    function information()
+    {
+        return array(
+            "name" => "",
+            "trigger" => array(""),
+            "information" => ""
+        );
+    }
+}
